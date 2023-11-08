6ce8f8ff71c8df556ee6f005f7a7a1e19dbad4d2
Ayodeji10 <eniolakayode38@gmail.com>
2023-11-08 12:51:38 +0100

    dev third commit

--- src/dev.js
+++ src/dev.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function DevBranchFile() {
-  return <div>DevBranchFile, commit 2</div>;
+  return <div>DevBranchFile, commit 2, commit 3</div>;
 }
 
 export default DevBranchFile;
