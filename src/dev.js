ecfbdb13739f0131f6f6aafa9d344e33c5ca47c3
unknown <eniolakayode38@gmail.comm>
2023-11-08 14:00:15 +0100

    dev commit 5

--- src/dev.js
+++ src/dev.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function DevBranchFile() {
-  return <div>DevBranchFile, commit 2, commit 3, commit 4</div>;
+  return <div>DevBranchFile, commit 2, commit 3, commit 4, commit 5</div>;
 }
 
 export default DevBranchFile;
