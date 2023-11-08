ebf2fcc995b6518abd591d46e02f62d00d0aaafc
Eniola <69263854+Ayodeji10@users.noreply.github.com>
2023-11-08 12:54:27 +0100

    dev commit 4

--- src/dev.js
+++ src/dev.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function DevBranchFile() {
-  return <div>DevBranchFile, commit 2, commit 3</div>;
+  return <div>DevBranchFile, commit 2, commit 3, commit 4</div>;
 }
 
 export default DevBranchFile;
